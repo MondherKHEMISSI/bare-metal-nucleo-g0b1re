a013310bc48b7df66894403b3492172d0f7b4b29
MondherKHEMISSI <monder850@gmail.com>
2023-12-29 16:53:13 +0100

    Added a simple bootloader

---
+++ bootloader/src/system.c
@@ -0,0 +1,31 @@
+#include "system.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
+
+volatile uint64_t ticks = 0;
+
+void sys_tick_handler(void) {
+  ticks++;
+}
+
+static void rcc_setup(void) {
+  rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_PLL_64MHZ]);
+}
+
+static void systick_setup(void) {
+  systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+  systick_counter_enable();
+  systick_interrupt_enable();
+}
+
+uint64_t system_get_ticks(void) {
+  return ticks;
+}
+
+void system_setup(void) {
+    rcc_setup();
+    systick_setup();
+}
+
